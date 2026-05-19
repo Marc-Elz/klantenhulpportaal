79ea02d5777121e291f7d82e566bf22045c96a78
Marc-E <marcelzinga@live.nl>
2026-05-19 15:22:14 +0200

    fixed typescript error

--- resources/js/services/http/index.ts
+++ resources/js/services/http/index.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { storeType } from "../store/storeTypes";
 
 const http = axios.create({
     baseURL: "/api",
@@ -11,7 +12,8 @@ axios.defaults.withCredentials = true;
 axios.defaults.withXSRFToken = true;
 
 export const getRequest = (endpoint: string) => http.get(endpoint);
-export const postRequest = (endpoint: string, data) =>
+export const postRequest = (endpoint: string, data: storeType) =>
     http.post(endpoint, data);
-export const putRequest = (endpoint: string, data) => http.put(endpoint, data);
+export const putRequest = (endpoint: string, data: storeType) =>
+    http.put(endpoint, data);
 export const deleteRequest = (endpoint: string) => http.delete(endpoint);
