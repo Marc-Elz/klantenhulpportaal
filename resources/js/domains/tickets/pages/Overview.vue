a81dc9b3d0c544232e39af68b8c60791c16c6d7a
Marc-Elz <marcelzinga@live.nl>
2026-06-02 21:27:22 +0200

    Fix show only names of categories

--- resources/js/domains/tickets/pages/Overview.vue
+++ resources/js/domains/tickets/pages/Overview.vue
@@ -19,7 +19,7 @@
                 <tr v-for="ticket in getAllTickets" :key="ticket.id">
                     <td>{{ ticket.id }}</td>
                     <td>{{ ticket.title }}</td>
-                    <td>{{ ticket.category_ids }}</td>
+                    <td>{{ ticket.category_ids.map((c: {id: number; name: string}) => c.name).join(', ') }}</td>
                     <!-- now gets a collection, should only show the names of categories-->
                     <td :style="{ color: getColor(ticket.status) }">
                         {{ ticket.status }}
