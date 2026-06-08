8c0db498181397e55c6342e61243f16e51d99518
Marc-Elz <marcelzinga@live.nl>
2026-06-08 15:56:27 +0200

    WIP somewhat less raw comments

--- resources/js/domains/comments/components/TicketDiscussion.vue
+++ resources/js/domains/comments/components/TicketDiscussion.vue
@@ -1,11 +1,17 @@
 <template>
     (Comments made by admins visible for submitter) (If user is admin show
     comment box for creating comment)
+
     <li v-for="comment in getAllComments" :key="comment.id">
-        {{ comment }}
+        {{ comment.content }}
+        <br />
+        {{ getUserById(comment.user_id).value.name }}
+        <br />
+        {{ comment.updated_at }}
     </li>
 </template>
 
 <script setup lang="ts">
+import { getUserById } from "../../users/store";
 import { getAllComments } from "../store";
 </script>
