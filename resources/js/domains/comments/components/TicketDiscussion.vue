841438b8afe2afea5e964765ae80bd00f9847bc5
Marc-E <marcelzinga@live.nl>
2026-06-09 10:32:55 +0200

    Remove template text for dev

--- resources/js/domains/comments/components/TicketDiscussion.vue
+++ resources/js/domains/comments/components/TicketDiscussion.vue
@@ -1,7 +1,4 @@
 <template>
-    (Comments made by admins visible for submitter) (If user is admin show
-    comment box for creating comment)
-
     <li v-for="comment in getCommentsSorted" :key="comment.id">
         {{ comment.content }}
         <br />
